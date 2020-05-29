37d05657ceb7750a80243d3efe424ccd40b62827
AttoCat <wazasiyo2525a@gmail.com>
2020-05-29 16:20:22 +0900

    3回でlimitになるように、簡単に復元できるように。

--- cogs/check.py
+++ cogs/check.py
@@ -17,14 +17,23 @@ class check(commands.Cog):
             member = message.author
             seigen = message.guild.get_role(714733639505543222)
             normal = message.guild.get_role(711375295172706313)
+            tyuui = message.guild.get_role(715809531829157938)
+            keikoku = message.guild.get_role(715809422148108298)
             await message.delete()
             embed = discord.Embed(
                 title="Message deleted",
                 description=f"""暴言が含まれていたため、削除しました。
                 {str(message.author)}:{message.content}""",
                 color=0xff0000)
-            await member.remove_roles(normal)
-            await member.add_roles(seigen)
+            if tyuui in member.roles:
+                await member.add_roles(keikoku)
+                await member.remove_roles(tyuui)
+            elif keikoku in member.roles:
+                await member.remove_roles(normal)
+                await member.add_roles(seigen)
+                await member.remove_roles(keikoku)
+            else:
+                await member.add_roles(tyuui)
             await message.channel.send(
                 embed=embed)
 
--- cogs/management.py
+++ cogs/management.py
@@ -7,17 +7,25 @@ class management(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.guild = self.bot.get_guild(711374787892740148)
+        self.seigen = self.guild.get_role(714733639505543222)
+        self.normal = self.guild.get_role(711375295172706313)
+        self.unei = self.guild.get_role(713321552271376444)
+
     @commands.command()
+    @commands.has_role(713321552271376444)
     async def protect(self, ctx, member: discord.Member):
-        guild = self.bot.get_guild(711374787892740148)
-        unei = guild.get_role(713321552271376444)
-        if unei not in ctx.author.roles:
-            await ctx.channel.send("あなたにはその権限がありません。")
-            return
-        seigen = guild.get_role(714733639505543222)
-        normal = guild.get_role(711375295172706313)
-        await member.remove_roles(normal)
-        await member.add_roles(seigen)
+        await member.remove_roles(self.normal)
+        await member.add_roles(self.seigen)
+        await ctx.message.delete()
+
+    @commands.command()
+    @commands.has_role(713321552271376444)
+    async def fix(self, ctx, member: discord.Member):
+        await member.remove_roles(self.seigen)
+        await member.add_roles(self.normal)
         await ctx.message.delete()
 
 
